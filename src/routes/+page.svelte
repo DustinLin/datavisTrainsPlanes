ea7bfe9a3b86916a83838df8c45ad5bcb4b8db12
haneen-225 <haneen.abderrazzaq@gmail.com>
2024-04-23 05:13:41 -0400

    wrote all the beautiful prose needed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
 	import MapStatic from './MapStatic.svelte';
 	import RailMapIntersect from './RailMapIntersect.svelte';
 
-	import { cutoffs, cityPairsToCities, roundTo } from '../utils';
+	import { cutoffs, cityPairsToCities, roundTo, planeTime } from '../utils';
 	import Histogram from './Histogram.svelte';
 
 
@@ -96,16 +96,24 @@
 
 <div class="container">
 	<div class="header">
-		<h3>Header class to put stuff?</h3>
+		<h3>High Speed Rail (HSR) in the US</h3>
 	</div>
 
 
 	<div class="main">
-		<h2>How do people travel between cities in the US</h2>
+		<h2>How do people travel long distances in the US?</h2>
 		<div class="infoMap" id="allRoutes">
 			<Map map={usaGeoContig} cities={manyRouteCities} cityCordMap={cityCordMap} onhover={onhover} showCityName={false} dims={[975, 610]}/>
 			<RouteDisplay highlightedRoute={highlightedRoute}/>
 		</div>
+
+		<!-- intro paragraph ab how ppl usually fly places-->
+		<p>
+			One of the most common, and seemingly most convenient, ways to travel relatively long distances within the United States is by flying. In 2023, there were {filteredCityPairToInfo.length} unique flight routes within the continental US. 
+			There are typically several flight options to travel between any two cities in the US that make flying an easy, flexible option. 
+			In comparison, driving several hours is often not a viable option, especially for short trips where time spent driving would nearly equal time spent at the actual destination. 
+			Currently, options for rail travel are limited in many parts of the country and, due to the lack of high speed rail (HSR) in the US, taking a train does not save significant time compared to driving.
+		</p>
 
 		<!-- compoenent -->
 		<h2>What are the popular airline routes in the US?</h2>
@@ -123,19 +131,41 @@
 			<Histogram dataset={filteredCityPairToInfo} xLabel={"Passengers (in millions)"} color={'#88aed0'} triangleColor={'#88aed0'}/> 
 		</div>
 
+		<!-- paragraph after bar chart of travel time breakdown that explains all the extra time spent when flying-->
+		<p>
+			Besides the time spent in the air on a plane, there are several additional steps involved in air travel that, in total, can add hours to a trip. 
+			One major inconvenience associated with flying is TSA — the restrictions and the time it takes to go through TSA security at the airport. 
+			Security lines can be very long, especially when the airport is very busy. 
+			Since no one can anticipate how long the wait in the security line will be, TSA recommends that people arrive at the airport two hours before their flight. 
+			Thus, even if the security line is short, travelers may still end up spending over an hour at the airport waiting to board. 
+			Additionally, commuting to and from the airport may total to an hour or more for people that live outside of major cities. 
+			Finally, although the actual air travel is fast, passengers also end up spending significant time on the airplane before takeoff and after landing for various reasons.
+		</p>
 
 		<h2>Is there a faster way?</h2>
+
+		<!-- paragraph before time triangle that introduces HSR-->
+		<p>
+			When factoring in time spent waiting at the airport or on the plane, along with time it takes to get to and from the airport, there are indeed faster alternatives to flying for certain distances of travel. 
+			For example, the fastest way to get across the world is flying, while the fastest way to get to the other side of town is likely to drive. 
+			Traveling a distance between these two extremes lends itself to rail travel, specifically high speed rail (HSR). 
+			The fastest train in the US, Amtrak's Acela line, has an average speed of 70 MPH (113 km/hr). 
+			This does not meet the required average speed of 124 MPH (200 km/hr) to be classified as high speed by the International Union of Railways. 
+			Thus, the United States currently has no functional HSR, despite the prevalence and success of HSR in many other nations including Japan and several European countries such as the UK, France, Italy, and Germany. 
+		</p>
+
 		<div class="infoMap" id="timeTriangle">
-			<p>
-				The answer is that it depends on how far you are traveling.
-				The fastest way to get across the world is flying, the fastest way to get to the other side of town is likely to drive (but could be an electric bike or subway depending on where you live). 
-				In between those distance there is sweet spot where the fastest mode of travel is High Speed Rail (HSR).
-				<br>
-				To find this sweet spot we find a line of best fit for each of the modes of transportation (Car, HSR, Planes). 
-				We very creatively call the area intersection formed by these lines the time triangle which is plotted to the right.
-				From this we find that when traveling between {roundTo(cutoffs.triangleLower, 0.1)} miles and {roundTo(cutoffs.triangleUpper, 0.1)} miles HSR is the fastest mode of transportation.
-			</p>
+
+
+			<!-- paragraph explaining time triangle that goes next to it-->
 			<TimeTriangle/>
+			<p>
+				To assess the potential benefits of implementing HSR in the US, we first determine the range of distances within which the fastest mode of travel would be HSR. 
+				To calculate this sweet spot, we find a line of best fit for each of the three modes of transportation (car, HSR, planes) considered here. 
+				We call the area formed by the intersection of these lines the “Time Triangle,” which is plotted to the right. 
+				The Time Triangle indicates that for distances between {roundTo(cutoffs.triangleLower, 0.1)} miles and {roundTo(cutoffs.triangleUpper, 0.1)} miles HSR is the fastest mode of transportation when factoring in all the main steps included with each method of travel. 
+				These distances correspond to flight times of {roundTo(planeTime(cutoffs.triangleLower), 0.1)} minutes and {roundTo(planeTime(cutoffs.triangleUpper), 0.1)} minutes, respectively. We show some of the airline routes that would be traveled quicker using HSR below. 
+			</p>
 		</div>
 
 		<h2>Airline routes that would be faster on HSR</h2>
@@ -147,8 +177,6 @@
 		</div>
 		<Histogram dataset={filteredCityPairToInfo} xLabel={"Passengers (in millions)"} color={'#88aed0'} triangleColor={'#cfe6ce'}/> 
 			
-
-		<p>The United States currently has no functional high speed rail. The fastest train in the US, Amtrak's Acela line, top speed of 160 MPH (257 km/hr) meets the International Union of Railways definition of travel at least 155 MPH (250 km/hr). However, the Acela average speed of 70 MPH (113 km/hr) does not meet the required average speed of 124 MPH (200 km/hr)</p>
 		
 
 
@@ -166,26 +194,23 @@
 		<div class="infoMap" id="iterativeGravityExplanation">
 			<div class="paragraphWrapper">
 				<p>
-				We propose 30 HSR rail lines with an Iterative Gravity Model.
-				This model is a modification we made to the Gravity Model presented in City Nerd's youtube video <a href="https://www.youtube.com/watch?v=wE5G1kTndI4">"56 High Speed Rail Links We Should've Built Already."</a>
+				To determine which of the potential rail lines would be the most impact, and thus should be focused on first, we built a mathematical model called an Iterative Gravity Model.
+				We propose 30 HSR lines that identified with by our model which was a modification we made to the Gravity Model presented in a YouTube video by CityNerd titled <a href="https://www.youtube.com/watch?v=wE5G1kTndI4">"56 High Speed Rail Links We Should've Built Already."</a>
 				Highly recommend checking out his video as it was the inspiration for our time triangle and iterative gravity model.
-				The gravity model as he presented is modeled after the equation for gravity. ADD MORE
+				The gravity model as he presented is modeled after the equation for gravity.
 				<br>
-				We feel it is necessary to expand on this basic gravity model in two ways:
+				We improve upon this basic gravity model in two ways:
 				</p>
 				<ol>
 					<li>
-						For the attractive force instead of using population as a proxy for where people want to travel to instead use a figure that isn't a proxy - total annual passengers from US flight data.
-					</li>
+						For the part of the equation representing “attractive force,” instead of using population as a proxy for where people want to travel, we use an actual figure that represents this: total annual passengers from US flight data.					</li>
 					<li>
-						Is to make the model aware of already proposed rail. <br>
-						To explain what we mean for the model to be aware consider a hypothetical when deciding where I should put a new HSR connection when some lines are already proposed.
-						Suppose New York City to Boston was already proposed. 
-						Now the model is considering connecting Washington DC to Boston.
-						Our first issue with the basic gravity model is when it computes the score for connecting Washington DC to Boston is considers building a line the entire way when only building to New York City is required.
-						The second thing we feel is an issue with the basic model is it doesn't take into consideration the fact that not only did I connect Washington DC to Boston I also connected Boston to New York City.
+						The model is aware of already proposed rail when determining where rail should be placed next. <br>
+						Consider the following hypothetical: 
+						Suppose rail between New York City and Boston was already proposed, and now the model is considering connecting Washington DC to Boston.
+						The basic gravity model, when computing the score for connecting Washington DC to Boston, considers building a line the entire way from Washington DC to Boston when only building to New York City is required since New York City and Boston have already been connected by HSR.
 						<br><br>
-						The Iterative Gravity model fixes both of these issues by first considering the direct connection and all possibilities of connecting two cities using existing rails. Secondly the score is now the sum of all scores for all the newly connected cities divided by the length of newly built rail line built.
+						The Iterative Gravity model fixes both of these issues by first considering the direct connection and all possibilities of connecting two cities using existing rails. Secondly, the score is now the sum of all scores for all the newly connected cities divided by the length of newly built rail line built.
 					</li>
 				</ol>
 			</div>
@@ -200,8 +225,13 @@
 		
 		<h2>Intersection with current rail</h2>
 
-		<p>The United states currently has no functional high speed rail. The fastest train in the US, Amtrak's Acela line, top speed of 160 MPH (257 km/hr) meets the International Union of Railways definition of travel at least 155 MPH (250 km/hr). However, the Acela average speed of 70 MPH (113 km/hr) does not meet the required average speed of 124 MPH (200 km/hr)</p>
-		<p>Consider making the above information partly into a table? Comparing speeds?</p>
+		<!-- paragraph talking ab current rail-->
+		<p>
+			As explained above, while Amtrak is the fastest rail in the US, it does not meet the qualification for HSR. 
+			Nonetheless, we can use the locations of these rail lines to guide future plans for building HSR using the existing infrastructure. 
+			The below maps show our proposed HSR routes (left) and the existing Amtrak rail lines (right), with all the cities identified with our Iterative Gravity Model labeled.
+		</p>
+
 		<div class="infoMap" id="railIntersection">
 			<RailMapIntersect map={usaGeoContig} cities={GravTopRes} cityCordMap={cityCordMap} cityAmtrakRouteMap={cityAmtrakRouteMap} dims={[800,500]}/>
 			<RailMap map={usaGeoContig} railMap={amtrakMapSimp} cityCordMap={cityCordMap} citiesPlotSet={citiesPlotSet} dims={[800,500]}/>
